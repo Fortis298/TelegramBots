2dfb2b345e0f0b47b8441efa8adf0a52f2de2647
Fortis298 <maxsimkolosov7@gmail.com>
2026-01-07 21:36:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,10 @@ import os
 from bot_currency import bot_cur
 from bot_notes import bot_notes
 
-WEBHOOK_BOT_CUR = os.getenv('WEBHOOK_BOT_CUR')
-WEBHOOK_BOT_NOTES = os.getenv('WEBHOOK_BOT_NOTES')
-
-webhoo_url_bot_cur = f"{WEBHOOK_BOT_CUR}{bot_cur}"
-webhoo_url_bot_notes = f"{WEBHOOK_BOT_NOTES}{bot_notes}"
-
 app = Flask(__name__)
 
 @app.route(f'/webhook/bot_cur', methods=['POST'])
-def webhook():
+def webhook_cur():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
     bot_cur.process_new_updates([update])
@@ -22,7 +16,7 @@ def webhook():
 
 
 @app.route(f'/webhook/bot_notes', methods=['POST'])
-def webhook():
+def webhook_notes():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
     bot_notes.process_new_updates([update])
@@ -30,9 +24,5 @@ def webhook():
 
 
 if __name__ == "__main__":
-    bot_cur.remove_webhook()
-    bot_cur.set_webhook(url=webhoo_url_bot_cur)
-    
-    
-    bot_notes.remove_webhook()
-    bot_notes.set_webhook(url=webhoo_url_bot_notes)
+   port = int(os.getenv("PORT", 10000))
+   app.run(host="0.0.0.0", port=port)
